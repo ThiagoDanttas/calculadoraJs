41db2bb1211f84f5c002fe67751d9cbba4c1971d
Thiago C Dantas <60719786+ThiagoDanttas@users.noreply.github.com>
2021-10-02 18:25:07 -0300

    first Commit

---
+++ Js/script.js
@@ -0,0 +1,25 @@
+function calcular(tipo, valor) {
+  if (tipo === "acao") {
+    if (valor === "c") {
+      //limpar o visor
+      document.getElementById("resultado").value = "";
+    }
+
+    if (
+      valor === "+" ||
+      valor === "-" ||
+      valor === "*" ||
+      valor === "." ||
+      valor === "/"
+    ) {
+      document.getElementById("resultado").value += valor;
+    }
+
+    if (valor === "=") {
+      const valorCampo = eval(document.getElementById("resultado").value);
+      document.getElementById("resultado").value = valorCampo;
+    }
+  } else if (tipo === "valor") {
+    document.getElementById("resultado").value += valor;
+  }
+}
